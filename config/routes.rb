89712861d0058097b4e6ca168287e85b15e84458
James Smith <james@floppy.org.uk>
2011-10-14 10:51:31 +0100

    Make question#new the default action

--- config/routes.rb
+++ config/routes.rb
@@ -50,9 +50,7 @@ Askamee::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "question#new"
 
   # See how all your routes lay out with "rake routes"
 
