0f5db98ed9a64d8004a4705e7d20e38f04833fb4
James Smith <james@floppy.org.uk>
2011-10-14 15:54:48 +0100

    Check categories for compatible inputs

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -11,7 +11,7 @@ class QuestionController < ApplicationController
         str
       end
     end
-    @quantities = content.select{|x| x.is_a? Quantity}
+    @quantity = content.find{|x| x.is_a? Quantity}
     @terms = TermExtract.extract(content.select{|x| x.is_a? String}.join(' '), :min_occurance => 1).map{|x| x[0]}
     ignore = [
       "emissions",
@@ -20,23 +20,28 @@ class QuestionController < ApplicationController
     @terms.delete_if {|x| ignore.include? x }
     
     # Find some AMEE categories that look relevant
-    @categories = AMEE::Search.new(AMEE::Rails.connection, :q => @terms.join(" "), :types=>'DC', :resultMax => 10, :matrix => 'itemDefinition', :excTags=>'ecoinvent') do |y|
-      x = y.result
-      passing = x.meta.wikiname &&
-        !x.meta.deprecated? && 
-        x.item_definition
-      item = passing ? x.data_items(:resultMax => 1, :matrix => 'label').first : nil
-      if passing && item
-        [x, item]
+    @categories = AMEE::Search.new(AMEE::Rails.connection, :q => @terms.join(" "), :types=>'DC', :resultMax => 10, :matrix => 'itemDefinition;path', :excTags=>'ecoinvent') do |y|
+      # Get category
+      cat = y.result
+      passing = cat.meta.wikiname &&
+        !cat.meta.deprecated? && 
+        cat.item_definition
+      # Check IVD
+      if passing
+        ivds = cat.item_definition.item_value_definition_list.sort{|a,b| a.path<=>b.path}
+        ivds = ivds.select{|x| x.profile? && x.versions.any?{|y| y=~/2/}}
+        ivd = ivds.find{|x| x.unit && (@quantity.unit.label == x.unit || @quantity.unit.alternatives_by_label.include?(x.unit)) } 
+      end
+      # Get data item
+      item = passing ? cat.data_items(:resultMax => 1, :matrix => 'label').first : nil
+      # Return results
+      if passing && item && ivd
+        [cat, item, ivd]
       else
         nil
       end
     end
-    
-    
-    #.find{|x| x.supports_auto_gallery? == true}
-    #puts category.map {|x| x.has_item_definition?}
-    
+
   end
 
 end
