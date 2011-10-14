b741097cc3a297ba67a7458d35cabcd36e39b004
James Smith <james@floppy.org.uk>
2011-10-14 10:41:19 +0100

    Initial framework generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Askamee::Application.config.secret_token = 'f4ece9214a2ec87e61f4dbce93751613645514346fa9a7cac8d83d0657db1b27297acec4bb6ec95649ef818b7474c1f3d308d130996ccd1f04c9f0fc3a463230'
