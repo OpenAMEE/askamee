47b98ccd4e7f91f9b5fec5895f07cca356da40df
James Smith <james@floppy.org.uk>
2012-02-02 16:22:35 +0000

    Add spec test for query parser

---
+++ spec/libs/query_parser_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'query_parser'
+
+describe QueryParser do
+
+  include QueryParser
+
+  {
+
+    '10kg of beef' => {
+      :inputs => ['10.0 kg'],
+      :terms => ['beef']
+    },
+    '10 kg beef' => {
+      :inputs => ['10.0 kg'],
+      :terms => ['beef']
+    },
+    '100km in a car' => {
+      :inputs => ['100.0 km'],
+      :terms => ['car']
+    },
+    '42kWh of electricity in the UK' => {
+      :inputs => ['42.0 kWh'],
+      :terms => ['electricity', 'UK']
+    },
+    '10 cows' => {
+      :inputs => ['10.0 '],
+      :terms => ['cows']
+    },
+    '1 long haul flight' => {
+      :inputs => ['1.0 '],
+      :terms => ['long', 'haul', 'flight']
+    },
+        
+  }.each_pair do |query, results|
+    
+    it "should parse '#{query}' correctly" do
+      # Parse string
+      inputs, terms = parse_query(query)
+      # Check inputs are parsed correctly
+      inputs.map{|x| x.to_s}.sort.should eql results[:inputs].sort
+      # Check remaining terms are correct
+      terms.sort.should eql results[:terms].sort
+    end
+    
+  end
+
+
+end
