ff4040a44b64a7d363ec7bab6d94692809e99e7f
James Smith <james@floppy.org.uk>
2012-02-03 10:47:13 +0000

    Update test to not include deprecated categories

--- spec/controllers/question_controller_spec.rb
+++ spec/controllers/question_controller_spec.rb
@@ -15,12 +15,8 @@ describe QuestionController do
 
     it "assigns categories" do
       get :answer, :q => 'shipping 10 tonnes of stuff for 1000 kilometres'
-      assigns(:categories).should eql [ "Ship_Freight_Defra", 
-                                        "Etching_and_CVD_cleaning_in_the_Electronics_Industry", 
-                                        "Ship_generic_Defra", 
-                                        "DEFRA_methodologies_for_transport", 
+      assigns(:categories).should eql [ "Etching_and_CVD_cleaning_in_the_Electronics_Industry", 
                                         "DEFRA_freight_transport_methodology", 
-                                        "Other_regional_ship_freight", 
                                         "Freight_transport_by_Greenhouse_Gas_Protocol" ]
     end
 
