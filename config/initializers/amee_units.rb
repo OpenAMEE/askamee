67c0d3c770be9a70b127c41bc7da3bd0ef8a6175
Chris Adams <wave@chrisadams.me.uk>
2012-03-30 15:47:40 +0100

    Replace read me with notes from code tour

--- config/initializers/amee_units.rb
+++ config/initializers/amee_units.rb
@@ -3,6 +3,9 @@ require 'quantify'
  
 Quantify::Unit.configure do
 
+  # this stops this string breaking AMEE
+  # "40 miles in a car"
+  # Will be interpreted as 40 mile-inch angstroms
   unload([:are, :inch])
   
 end
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,5 +1,8 @@
 ActiveSupport::Inflector.inflections do |inflect|
+  # Rails uses the old english for plural of cows, kine
+  # override to stop comedy naming errors
   inflect.plural 'cow', 'cows'
   inflect.singular 'cows', 'cow'
   inflect.uncountable 'cattle'
+  inflect.singular 'minibuses', 'minibus' # singular is a minibu otherwise
 end
